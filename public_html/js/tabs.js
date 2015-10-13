9f6600e79ec74df0e6f6023b1196a2fa2ad13aac
Clint Hall <theaetetuslabs@gmail.com>
2015-10-13 10:53:32 -0400

    return mousehold.js to its own file

--- public_html/js/tabs.js
+++ public_html/js/tabs.js
@@ -1,61 +1,7 @@
-/**
- * jQuery mousehold plugin - fires an event while the mouse is clicked down.
- * Additionally, the function, when executed, is passed a single
- * argument representing the count of times the event has been fired during
- * this session of the mouse hold.
- *
- * @author Remy Sharp (leftlogic.com)
- * @date 2006-12-15
- * @example $("img").mousehold(200, function(i){  })
- * @desc Repeats firing the passed function while the mouse is clicked down
- *
- * @name mousehold
- * @type jQuery
- * @param Number timeout The frequency to repeat the event in milliseconds
- * @param Function fn A function to execute
- * @cat Plugin
- */
-
-jQuery.fn.mousehold = function(timeout, f) {
-    if (timeout && typeof timeout == 'function') {
-        f = timeout;
-        timeout = 100;
-    }
-    if (f && typeof f == 'function') {
-        var timer = 0;
-        var fireStep = 0;
-        return this.each(function() {
-            jQuery(this).mousedown(function() {
-                fireStep = 1;
-                var ctr = 0;
-                var t = this;
-                timer = setInterval(function() {
-                    ctr++;
-                    f.call(t, ctr);
-                    fireStep = 2;
-                }, timeout);
-            })
-
-            clearMousehold = function() {
-                clearInterval(timer);
-                if (fireStep == 1)
-                    f.call(this, 1);
-                fireStep = 0;
-            }
-
-            jQuery(this).mouseout(clearMousehold);
-            jQuery(this).mouseup(clearMousehold);
-        })
-    }
-}
-
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
 /*
  * Copyright B. Clint Hall 2014-2015.  All rights reserved.
  * Contact the author to discuss licensing.  theaetetus7  gmail.com
 */
-////////////////////////////////////////////////////////////////////////////////
 
 
 function TabManager() {
@@ -164,7 +110,6 @@ function TabManager() {
         function rightBtnHold() {
             var tabHeaders = $(this).siblings(".tabHeaders");
             var left = parseInt(tabHeaders.css("left"));
-            console.log(left);
             // keep right edge of last tab from moving left of right edge of right btn;
             if (rightEdgeOfLastTab(tabHeaders) > rightEdgeOfRightBtn(this) - 3) {
                 tabHeaders.css("left", left - 3 + "px");
